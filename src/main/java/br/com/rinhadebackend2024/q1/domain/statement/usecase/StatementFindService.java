a5fcf4d4b161880cbbd5090f7671b77920050aaf
mmrosatab <mmrosatab@hotmail.com>
2025-03-10 22:56:44 -0300

    chore: remove files

--- src/main/java/br/com/rinhadebackend2024/q1/domain/statement/usecase/StatementFindService.java
+++
@@ -1,4 +0,0 @@
-package br.com.rinhadebackend2024.q1.domain.statement.usecase;
-
-public class StatementFindService {
-}
--- src/main/java/br/com/rinhadebackend2024/q1/infra/http/controller/statement/StatementFindController.java
+++
@@ -1,4 +0,0 @@
-package br.com.rinhadebackend2024.q1.infra.http.controller.statement;
-
-public class StatementFindController {
-}
